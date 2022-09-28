bc9f17fdd63350791b74e6f4acf4e8a5c349b0ab
brad@smithwebtek.com <brad@smithwebtek.com>
2022-09-28 17:57:05 -0400

    Update client

---
+++ src/components/tutorialsList.component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TutorialsList = () => {
+
+  return (
+    <div>TutorialsList component</div>
+  )
+}
+
+export default TutorialsList;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
